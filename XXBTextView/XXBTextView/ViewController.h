1d97d807b72cfff35783f90f36573453186e1cb9
杨小兵 <six_tiger@163.com>
2015-04-22 11:31:56 +0800

    初始化项目
    一个有placehoder的textView

---
+++ XXBTextView/XXBTextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XXBTextView
+//
+//  Created by 杨小兵 on 15/4/22.
+//  Copyright (c) 2015年 xiaoxiaobing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XXBTextView/XXBTextView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  XXBTextView
+//
+//  Created by 杨小兵 on 15/4/22.
+//  Copyright (c) 2015年 xiaoxiaobing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
